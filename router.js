2333441d029438b46bd5d59f4e81c3e749f0c69c
cuiling.liu <cuiling.liu@baisonmail.com>
2019-03-29 10:13:44 +0800

    增加获取歌单列表的接口

--- router.js
+++ router.js
@@ -22,4 +22,35 @@ router.get('/recommend/swiper', async (req, res) => {
   res.send(data.data)
 })
 
+// 获取全部歌单数据
+router.get('/recommend/playlist', async (req, res) => {
+  let data = await axios({
+    method: 'get',
+    url: 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg',
+    params: {
+      picmid: 1,
+      rnd: Math.random(),
+      g_tk: 5381,
+      loginUin: 0,
+      hostUin: 0,
+      format: 'json',
+      inCharset: 'utf8',
+      outCharset: 'utf-8',
+      notice: 0,
+      platform: 'yqq.json',
+      needNewCode: 0,
+      categoryId: 10000000,
+      sortId: 5,
+      sin: 0,
+      ein: 19
+    },
+    headers: {
+      origin: 'https://y.qq.com',
+      Referer: 'https://y.qq.com/portal/playlist.html'
+    }
+  })
+  // console.log(data.data)
+  res.send(data.data)
+})
+
 module.exports = router
